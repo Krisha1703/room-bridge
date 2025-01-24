425fc4fb7a338bd055b76fee6aba3696008c16c4
krish <krishabotadara@gmail.com>
2025-01-24 21:51:26 +0700

    verify user

--- src/app/verify-user/page.jsx
+++ src/app/verify-user/page.jsx
@@ -1,5 +1,5 @@
 "use client"
-import { useState, useEffect } from "react";
+import { useState, useEffect, Suspense } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 const VerifyUserPage = () => {
@@ -17,12 +17,12 @@ const VerifyUserPage = () => {
     }
   }, [searchParams]);
 
-  console.log("Token from url ",token);
+  console.log("Token from url ", token);
 
   const handleVerify = async () => {
     if (!token) {
       setStatus("Invalid or missing token.");
-      console.log("Token from url ",token);
+      console.log("Token from url ", token);
       return;
     }
 
@@ -57,4 +57,10 @@ const VerifyUserPage = () => {
   );
 };
 
-export default VerifyUserPage;
+const SuspenseWrapper = () => (
+  <Suspense fallback={<div>Loading...</div>}>
+    <VerifyUserPage />
+  </Suspense>
+);
+
+export default SuspenseWrapper;
